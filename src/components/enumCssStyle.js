b40dc3322db582cfa582d481afff8288a336e9a2
mksgdwn <mksgdwn@gmail.com>
2023-04-30 07:45:48 +0500

    feat: add enumCssStyle module

---
+++ src/components/enumCssStyle.js
@@ -0,0 +1,10 @@
+const enumCssStyle = {
+  KEYBOARD: 'keyboard',
+  BTN: 'btn',
+  ROW: 'row',
+  UPPER_CASE: 'upperCase',
+  LOWER_CASE: 'lowerCase',
+  HIDDEN: 'hidden',
+};
+
+export default enumCssStyle;
