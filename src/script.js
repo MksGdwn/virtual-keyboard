d46a2a766380ab5dde51a545fa6b8392908d5f7a
mksgdwn <mksgdwn@gmail.com>
2023-04-30 08:44:59 +0500

    feat: add textarea

--- src/script.js
+++ src/script.js
@@ -4,7 +4,14 @@ import createElement from './components/utils.js';
 const body = document.querySelector('body');
 const main = document.createElement('main');
 const wrapper = createElement('div', ['wrapper']);
+
+const textarea = createElement('textarea', ['textarea']);
+textarea.setAttribute('rows', 10);
+textarea.setAttribute('cols', 50);
+
 const keyboard = new Keyboard();
+
 body.append(main);
 main.append(wrapper);
+wrapper.append(textarea);
 wrapper.append(keyboard.keyboard);
