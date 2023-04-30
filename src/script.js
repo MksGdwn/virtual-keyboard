afe8062c78efb6cdec0abe3316512f0059a17f51
mksgdwn <mksgdwn@gmail.com>
2023-04-30 09:10:48 +0500

    feat: add text input from keyboard

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import Keyboard from './components/keyboard.js';
 import createElement from './components/utils.js';
+import enumCssStyle from './components/enumCssStyle.js';
 
 const body = document.querySelector('body');
 const main = document.createElement('main');
@@ -15,3 +16,44 @@ body.append(main);
 main.append(wrapper);
 wrapper.append(textarea);
 wrapper.append(keyboard.keyboard);
+
+const keys = document.querySelectorAll(`.${enumCssStyle.BTN}`);
+
+function inputNewText(index, value, text) {
+  const result = value.slice(0, index) + text + value.slice(index);
+
+  return result;
+}
+
+keys.forEach((key) => {
+  key.addEventListener('click', () => {
+    if (key.innerText.length === 1) {
+      const start = textarea.selectionStart;
+      textarea.value = inputNewText(start, textarea.value, key.innerText);
+      textarea.setSelectionRange(start + 1, start + 1);
+    }
+  });
+});
+
+body.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  keys.forEach((key) => {
+    if (key.classList.contains(event.code)) {
+      key.classList.add('active');
+
+      if (key.innerText.length === 1) {
+        const start = textarea.selectionStart;
+        textarea.value = inputNewText(start, textarea.value, key.innerText);
+        textarea.setSelectionRange(start + 1, start + 1);
+      }
+    }
+  });
+});
+
+body.addEventListener('keyup', (event) => {
+  keys.forEach((key) => {
+    if (key.classList.contains(event.code)) {
+      key.classList.remove('active');
+    }
+  });
+});
